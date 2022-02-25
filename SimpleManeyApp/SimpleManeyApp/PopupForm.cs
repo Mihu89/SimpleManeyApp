e69e6bbe7ce949eaba9c8cd9fc7d9601239b358c
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-02-25 20:09:51 +0200

    Validate Popup on Save

--- SimpleManeyApp/SimpleManeyApp/PopupForm.cs
+++ SimpleManeyApp/SimpleManeyApp/PopupForm.cs
@@ -19,7 +19,30 @@ namespace SimpleManeyApp
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-
+            decimal cost = 0;
+            if (string.IsNullOrEmpty(tbCost.Text))
+            {
+                MessageBox.Show("Fill cost");
+                tbCost.Focus();
+            }
+            else
+            {
+                bool isNumber = decimal.TryParse(tbCost.Text, out cost);
+                if (!isNumber)
+                {
+                    MessageBox.Show("Write a valid number");
+                }
+            }
+            if (string.IsNullOrEmpty(tbTitle.Text))
+            {
+                MessageBox.Show("Fill title");
+                tbTitle.Focus();
+            }
+            else
+            {
+                var operation = new Operation(1, tbTitle.Text, cost);
+                MessageBox.Show(operation.Title + " " + operation.Cost);
+            }
         }
     }
 }
